254dd5c85f0b3fcb12e7463b70cb5cc9e8ebc932
raolson83 <144298688+raolson83@users.noreply.github.com>
2024-03-25 19:02:50 -0600

    Fix to CM

---
+++ SQL wine table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE wine_components (
+    alcohol float,
+	malic_acid float,
+	ash float,	
+	alcalinity_of_ash float,
+	magnesium float,
+	total_phenols float,
+	flavanoids float,
+	nonflavanoid_phenols float,
+	proanthocyanins float,
+	color_intensity float,
+	hue float,
+	"od280/od315_of_diluted_wines" float,
+	proline float,
+	target integer
+);
